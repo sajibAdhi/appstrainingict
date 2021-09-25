c1f0889616ea32f912fd4fcd87219ef7c2487775
robinNcode <msmrobin518@gmail.com>
2021-09-25 11:05:14 +0600

    done

--- day9/inheritance_application/lib/inheritance_application.dart
+++ day9/inheritance_application/lib/inheritance_application.dart
@@ -1,14 +1,26 @@
 void main() {
   var cow = Cow();
+
+  print(cow.color);
   print(cow.age);
+  cow.noise();
 }
 
 class Animal {
   String color = 'Yellow';
+  void noise() {
+    print('Animal Noice Anything');
+  }
 }
 
 class Cow extends Animal {
   int age = 80;
+
+  void superNoice() {
+    super.noise();
+  }
+
+  @override
   void noise() {
     print('Cow sounds Hamba');
   }
